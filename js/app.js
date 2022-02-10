cd98e6c704e1a6c68e2503b70a26afa2ab03f234
Matheus da silva <ms25022003@gmail.com>
2022-02-10 16:04:03 -0300

    Implantação do modal

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-var value1 = document.getElementById('value1')
-var value2 = document.getElementById('value2')
-var result = document.getElementById('result')
-var h2 = document.querySelector('h2')
-var button = document.getElementById('calc')
-const inputs = document.getElementById('inputs')
-console.log(inputs)
+var value1 = document.getElementById('value1');
+var value2 = document.getElementById('value2');
+var result = document.getElementById('result');
+var h2 = document.querySelector('h2');
+var button = document.getElementById('calc');
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector('#close-modal');
 
 var imgs = document.querySelectorAll('img')
 imgs.forEach(element => {
     element.addEventListener('click', (e) => {
-        document.querySelector("section.inputs").style.display="flex";
-        console.log(e.target.id)
+        modal.style.display = "flex";
         switch (e.target.id) {
             case "Quadrado":
                 medirQuadrado()
@@ -21,13 +20,17 @@ imgs.forEach(element => {
             case "Triangulo":
                 medirTriangulo()
                 break;
-        
+
             default:
                 break;
         }
     })
 });
 
+closeModal.addEventListener('click', () => {
+        modal.style.display = "none"
+    }
+)
 //Valores para medir Quadrado
 var number1 = 10
 
@@ -41,34 +44,36 @@ const pi = 3.14
 
 // medir a area do quadrado
 function medirQuadrado() {
-    h2.innerText="Quadrado"
-    value1.style.display="flex";
-    value1.setAttribute('placeholder',"Lado")
-    value2.style.display="none";
-    button.addEventListener('click',()=>{
+    h2.innerText = "Quadrado"
+    value1.style.display = "flex";
+    value1.setAttribute('placeholder', "Lado")
+    value2.style.display = "none";
+    button.addEventListener('click', () => {
         result.value = value1.value ** 2
     });
 }
 // medir a area do triangulo
 function medirTriangulo() {
-    h2.innerText="Triângulo"
-    value1.style.display="flex";
-    value1.setAttribute('placeholder',"Base")
-    value2.style.display="flex";
-    value2.setAttribute('placeholder',"Altura")
-    button.addEventListener('click',()=>{
-            result.value = (value1.value * value2.value) / 2
-        });
+    h2.innerText = "Triângulo"
+    value1.style.display = "flex";
+    value1.setAttribute('placeholder', "Base")
+    value2.style.display = "flex";
+    value2.setAttribute('placeholder', "Altura")
+    button.addEventListener('click', () => {
+        result.value = (value1.value * value2.value) / 2
+    });
     // result.value = (base * altura) / 2
 }
 // medir a area do circulo
 function medirCirculo() {
-    h2.innerText="Circulo"
-    value1.style.display="flex";
-    value1.setAttribute('placeholder',"Raio")
-    value2.style.display="none";
-    button.addEventListener('click',()=>{
+    h2.innerText = "Circulo"
+    value1.style.display = "flex";
+    value1.setAttribute('placeholder', "Raio")
+    value2.style.display = "none";
+    button.addEventListener('click', () => {
         result.value = pi * (value1.value ** 2)
     });
     // result.value = pi * (raio ** 2)
 }
+
+
