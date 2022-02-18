f322e8779eeb6b91dda73f32353b322ffa47cf44
Matheus da silva <ms25022003@gmail.com>
2022-02-18 11:36:35 -0300

    Create resetInputs function, rename functions, variables.

--- js/app.js
+++ js/app.js
@@ -12,15 +12,17 @@ imgs.forEach(element => {
         modal.style.display = "flex";
         switch (e.target.id) {
             case "Quadrado":
-                medirQuadrado()
+                resetInputs();
+                calculateSquare();
                 break;
             case "Circulo":
-                medirCirculo()
+                resetInputs();
+                calculateCircle();
                 break;
             case "Triangulo":
-                medirTriangulo()
+                resetInputs();
+                calculateTriangle();
                 break;
-
             default:
                 break;
         }
@@ -31,19 +33,19 @@ closeModal.addEventListener('click', () => {
         modal.style.display = "none"
     }
 )
-//Valores para medir Quadrado
+//Valores a medir Quadrado
 var number1 = 10
 
-//Valores para medir Triangulo
+//Values to measure Triangle
 var base = 10
-var altura = 8
+var height = 8
 
-//Valores para medir Circulo
-var raio = 5
+//Values to measure Circle
+var radius = 5
 const pi = 3.14
 
-// medir a area do quadrado
-function medirQuadrado() {
+// Measure the area of the square
+function calculateSquare() {
     h2.innerText = "Quadrado"
     value1.style.display = "flex";
     value1.setAttribute('placeholder', "Lado")
@@ -52,28 +54,32 @@ function medirQuadrado() {
         result.value = value1.value ** 2
     });
 }
-// medir a area do triangulo
-function medirTriangulo() {
+// Measure the area of the triangle
+function calculateTriangle() {
     h2.innerText = "Triângulo"
     value1.style.display = "flex";
     value1.setAttribute('placeholder', "Base")
     value2.style.display = "flex";
-    value2.setAttribute('placeholder', "Altura")
+    value2.setAttribute('placeholder', "height")
     button.addEventListener('click', () => {
         result.value = (value1.value * value2.value) / 2
     });
-    // result.value = (base * altura) / 2
+    // result.value = (base * height) / 2
 }
-// medir a area do circulo
-function medirCirculo() {
+// Measure the area of the circle
+function calculateCircle() {
     h2.innerText = "Circulo"
     value1.style.display = "flex";
-    value1.setAttribute('placeholder', "Raio")
+    value1.setAttribute('placeholder', "radius")
     value2.style.display = "none";
     button.addEventListener('click', () => {
         result.value = pi * (value1.value ** 2)
     });
-    // result.value = pi * (raio ** 2)
+    // result.value = pi * (radius ** 2)
 }
-
-
+// Reset the inputs
+function resetInputs() {
+    value1.value = "";
+    value2.value = "";
+    result.value = ""
+}
